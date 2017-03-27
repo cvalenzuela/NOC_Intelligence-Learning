aedbd7564185178c2d53aab9c174c4bafde67493
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-03-27 01:17:34 -0400

    typos

--- assignment1/README.md
+++ assignment1/README.md
@@ -1,6 +1,6 @@
 # BFS with Wikipedia articles and words/terms
 
-Using a Breadth-First Algorithm (BFS), this script will search for the shortest way to get from one article in Wikipedia to a specific word or term used in another article.
+Using a Breadth-First Algorithm (BFS), this script will search for the first shortest way to get from one article in Wikipedia to a specific word or term used in another article.
 
 
 Install dependencies:
@@ -14,26 +14,33 @@ Run:
 Here is what the interactive prompt looks like:
 
 ```
-Enter the name of a Wikipedia article from where to start (ie: 'French Revolution'):  Jeff Koons
-Enter a term or word of search for (ie: 'economic freedoms'): science
+-----
+This script will search for the first shortest
+way to get from one article in Wikipedia to a
+specific word or term used in another article.
+-----
 
-Awesome!, I will search for the most efficent way to start from the Jeff Koons Wikipedia article
-and find the path to another article that talks about science.
+Ready? (y/n):  y
+Enter the name of a Wikipedia article from where to start (ie: 'Paul Cezanne'):  Paul Cezanne
+Enter a term or word of search for (ie: 'Gertrude Stein'): Gertrude Stein
+
+Awesome!, I will search for the most efficient way to start from the Paul Cezanne Wikipedia article
+and find the path to another article that talks about Gertrude Stein.
 
 This may take me a while, so chill...Ok?
 
-    Searching how to get from Jeff Koons to science.
+  Searching how to get from Paul Cézanne to Gertrude Stein.
 
 I found it!
-Here is the most efficent way:
+Here is the first shortest path I discovered:
 
-                Jeff Koons ---> American Academy of Arts and Sciences ---> the American Academy of Arts and Sciences Wikipedia article talks about science.!
+  Paul Cezanne ---> Ambroise Vollard ---> the Ambroise Vollard wikipedia article talks about Gertrude Stein!
+
 ```
 
-For articles and words that are not very related (like from 'Lionel Messi' to 'quantum mechanics') it may take a while. This works better with 'closer' articles and terms.
+For articles and words that are not very related (like from 'Lionel Messi' to 'quantum mechanics') it may take a while or crash due to API requests. This works better with 'closer' articles and terms. It also works better when it goes from specific to general. The results are in alphabetical order.
 
 ## References
---------
 
 This is based on [Grokking Algorithms Chapter 6](https://manning-content.s3.amazonaws.com/download/2/656dfe9-e8fa-49ac-ac80-46584976ff49/GrokkingAlgorithms_SampleChapter6.pdf) and [NOC-S17-2-Intelligence-Learning bfs examples](https://github.com/shiffman/NOC-S17-2-Intelligence-Learning/tree/master/week1-graphs/02_bfs_oop).
 
--- assignment1/bfs-wikipedia.py
+++ assignment1/bfs-wikipedia.py
@@ -27,11 +27,11 @@ if begin != 'y':
 else:
     pass
 
-start_article = raw_input("Enter the name of a Wikipedia article from where to start (ie: 'French Revolution'):  ")
-search_term = raw_input("Enter a term or word of search for (ie: 'economic freedoms'): ")
+start_article = raw_input("Enter the name of a Wikipedia article from where to start (ie: 'Paul Cezanne'):  ")
+search_term = raw_input("Enter a term or word of search for (ie: 'Gertrude Stein'): ")
 
 print('''
-Awesome!, I will search for the most efficent way to start from the %s Wikipedia article
+Awesome!, I will search for the most efficient way to start from the %s Wikipedia article
 and find the path to another article that talks about %s.
 ''' % (start_article, search_term))
 
@@ -92,39 +92,44 @@ def search(name):
     search_queue += graph[root_node.title] # add all of the root node urls to the search queue
     searched = [] # this array keeps track of which article we have already search for, so we dont search twice.
     while search_queue: # while the queue isn't empty
-        # sys.stdout.write(spinner.next())
-        # sys.stdout.flush()
-        # time.sleep(0.1)
-        # sys.stdout.write('\b')
+        # fancy spinner to wait
+        sys.stdout.write(spinner.next())
+        sys.stdout.flush()
+        time.sleep(0.1)
+        sys.stdout.write('\b')
         article = search_queue.popleft() # grabs the first article off the queue
         if not article.title in searched: # only search this article if we haven't already search for
-            article.get_article() # get the content from wikipedia
-            #print article.title
-            if search_term in article.content: # check if article contains the search_term we are looking for
-                all_nodes.append(article)
-                print 'I found it!'
-                print 'Here is the most efficent way:'
-                current = article.title
-                path = []
-                while current != start_article:
-                    for article in all_nodes:
-                        if article.title == current:
-                            path.append(article.title)
-                            current = article.parent
-                path.append(start_article)
-                print('''
-                %s ---> the %s Wikipedia article talks about %s.!
-                ''' % (' ---> '.join(path[::-1]), path[0], search_term))
-                #print ' ---> '.join(path[::-1]) + ' ---> ' + path[0] + ' has ' + search_term + ' in it!'
-                print 'cool!'
-                return True
-            else:
-                graph[article.title] = []
-                all_nodes.append(article)
-                for name in article.links:
-                    graph[article.title].append(Node(name, article.title)) # add all of the first article links to the graph
-                search_queue += graph[article.title]
-            searched.append(article.title) # mark this article as searched.
+            try:
+                article.get_article() # get the content from wikipedia
+                #print article.title
+                if search_term in article.content: # check if article contains the search_term we are looking for
+                    all_nodes.append(article)
+                    print 'I found it!'
+                    print 'Here is the first shortest path I discovered:'
+                    current = article.title
+                    path = []
+                    while current != root_node.title:
+                        for article in all_nodes:
+                            if article.title == current:
+                                path.append(article.title)
+                                current = article.parent
+                    path.append(start_article)
+                    print('''
+                    %s ---> the %s wikipedia article talks about %s!
+                    ''' % (' ---> '.join(path[::-1]), path[0], search_term))
+                    #print ' ---> '.join(path[::-1]) + ' ---> ' + path[0] + ' has ' + search_term + ' in it!'
+                    print 'cool!'
+                    return True
+                else:
+                    graph[article.title] = []
+                    all_nodes.append(article)
+                    for name in article.links:
+                        graph[article.title].append(Node(name, article.title)) # add all of the first article links to the graph
+                    search_queue += graph[article.title]
+                searched.append(article.title) # mark this article as searched.
+            except:
+                pass
+    print 'wops!'
     return False # if we reach here, the term was not find in the max of x iterations
 
 # start the search
